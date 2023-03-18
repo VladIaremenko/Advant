4d67bbd3ff507c6765bbd90e53d7fd3a0e8ec73b
Vlad <vlad.iaremenko@gmail.com>
2023-03-18 15:24:00 +0300

    Sagra updates

--- Assets/_Scripts/_General/PlayerMovementSystem.cs
+++ Assets/_Scripts/_General/PlayerMovementSystem.cs
@@ -1,7 +1,6 @@
 ﻿using Leopotam.EcsLite;
 using Leopotam.EcsLite.Di;
 using Sagra.Assets._Scripts._Components;
-using Sagra.Assets._Scripts._Fly._Components;
 
 namespace Sagra.Assets._Scripts._General
 {
