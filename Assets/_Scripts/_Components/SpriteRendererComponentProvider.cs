2664b58c70a1e6323542e772459f0e997e022aa1
Vlad <vlad.iaremenko@gmail.com>
2023-03-18 21:45:50 +0300

    Bg sprite

---
+++ Assets/_Scripts/_Components/SpriteRendererComponentProvider.cs
@@ -0,0 +1,14 @@
+﻿using Sagra.Assets._Scripts._Misc;
+using System;
+using UnityEngine;
+
+namespace Sagra.Assets._Scripts._Components
+{
+    public sealed class SpriteRendererComponentProvider : MonoProvider<SpriteRendererComponent> { }
+
+    [Serializable]
+    public struct SpriteRendererComponent
+    {
+        public SpriteRenderer SpriteRenderer;
+    }
+}
