54b4f0d645b5f498cd926beec7e66dea741993f3
Vlad <vlad.iaremenko@gmail.com>
2023-03-18 15:15:49 +0300

    Scene updates

--- Assets/_Scripts/_General/PlayerMovementSystem.cs
+++ Assets/_Scripts/_General/PlayerMovementSystem.cs
@@ -1,23 +1,18 @@
 ﻿using Leopotam.EcsLite;
 using Leopotam.EcsLite.Di;
+using Sagra.Assets._Scripts._Components;
 using Sagra.Assets._Scripts._Fly._Components;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 namespace Sagra.Assets._Scripts._General
 {
     public class PlayerMovementSystem : IEcsRunSystem
     {
-        readonly EcsFilterInject<Inc<TranfsormComponent>> _movingUnits = default;
+        readonly EcsFilterInject<Inc<TranfsormComponent, PlayerControlledComponent>> _movingUnits = default;
 
         public void Run(IEcsSystems systems)
         {
-            foreach (var entity in _movingUnits.Value)
-            {
-                ref var unit = ref _movingUnits.Pools.Inc1.Get(entity);
-
-                unit.Transform.position += Vector3.up * Time.deltaTime * 5;
-            }
+            
         }
     }
 }
