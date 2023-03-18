0fa450957b7f1687991e9e0584abbbfd9883e5e5
Vlad <vlad.iaremenko@gmail.com>
2023-03-18 22:18:46 +0300

    Items updates

---
+++ Assets/_Scripts/_General/ObstaclesSpawnSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsLite;
+
+namespace Sagra.Assets._Scripts._General
+{
+    internal class ObstaclesSpawnSystem : IEcsRunSystem
+    {
+        public void Run(IEcsSystems systems)
+        {
+            
+        }
+    }
+}
