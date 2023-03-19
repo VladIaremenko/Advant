8d45b1363c65a45d13f1550ac5599e084b276c3d
Vlad <vlad.iaremenko@gmail.com>
2023-03-19 12:53:36 +0300

    Refractor

--- Assets/_Scripts/_General/BackgroundSystem.cs
+++ Assets/_Scripts/_General/BackgroundSystem.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 
 namespace Sagra.Assets._Scripts._General
 {
-    public class BackgroundSystem : IEcsRunSystem
+    public class BackgroundSystem : IEcsInitSystem,IEcsRunSystem
     {
         readonly EcsFilterInject<Inc<SpriteRendererComponent>> _units = default;
 
-        public void Run(IEcsSystems systems)
+        private float _timeElapsed;
+
+        public void Init(IEcsSystems systems)
         {
             foreach (var entity in _units.Value)
             {
                 ref var unit = ref _units.Pools.Inc1.Get(entity);
 
+                unit.SpriteRenderer.material.SetTextureOffset("_MainTex",
+                    new Vector2(0, 0));
+            }
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            _timeElapsed += Time.deltaTime;
+
+            foreach (var entity in _units.Value)
+            {
+                ref var unit = ref _units.Pools.Inc1.Get(entity);
+
                 unit.SpriteRenderer.material.SetTextureOffset("_MainTex", 
-                    new Vector2(0, Time.time/100));
+                    new Vector2(0, _timeElapsed/100));
             }
         }
     }
--- Assets/_Scripts/_General/PlayerStateCheckSystem.cs
+++ Assets/_Scripts/_General/PlayerStateCheckSystem.cs
@@ -11,19 +11,34 @@ namespace Sagra.Assets._Scripts._General
         readonly EcsFilterInject<Inc<PlayerControlledComponent>> _playerUnits = default;
         readonly EcsPoolInject<UnitStateComponent> _unitStates = default;
 
+        private bool _gameIsOver = false;
+        private float _timeAfterReloadCount = 0;
+        private float _timeBeforeReload = 3;
+
         public void Run(IEcsSystems systems)
         {
+            if (_gameIsOver)
+            {
+                _timeAfterReloadCount += Time.deltaTime;
+
+                if(_timeAfterReloadCount >= _timeBeforeReload)
+                {
+                    SceneManager.LoadScene(0);
+                }
+
+                return;
+            }
+
             foreach (var entity in _playerUnits.Value)
             {
                 ref var unit = ref _unitStates.Value.Get(entity);
 
-                if (!unit.IsDead)
+                if (unit.IsDead)
                 {
+                    _gameIsOver = true;
                     return;
                 }
             }
-
-            SceneManager.LoadScene(0);
         }
     }
 }
